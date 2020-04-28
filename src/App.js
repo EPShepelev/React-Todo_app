3be2995c0902cb86f169e036e07ebae7e5367a0d
Shepelev Evgeniy <shepelev.ep@yandex.ru>
2020-04-28 11:39:15 +0300

    added modal

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from 'react'
 import TodoList from './Todo/TodoList'
 import Context from  './context'
-import AddTodo from './Todo/AddTodo';
+import Loader from './Loader'
+import Modal from './Modal/Modal'
+
+const AddTodo = React.lazy(() => import('./Todo/AddTodo'))
 
 function App() {
   const [todos, setTodos] = React.useState([])
+  const [loading, setLoading] = React.useState(true)
  
   useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/todos?_limit=5')
@@ -12,6 +16,7 @@ function App() {
     .then(todos => {
       setTimeout(() => {
         setTodos(todos)
+        setLoading(false)
       }, 2000)
     })
   }, [])
@@ -43,8 +48,16 @@ function App() {
     <Context.Provider value={{removeTodo}}>
     <div className='wrapper'>
       <h1>React tutorial</h1>
-      <AddTodo onCreate={addTodo}/>
-      {todos.length ? <TodoList todos={todos} onToggle={toggleTodo}/> : <p>No todos!</p>}
+      <Modal/>
+      <React.Suspense fallback={<p>Loading...</p>}>
+        <AddTodo onCreate={addTodo}/>
+      </React.Suspense>
+      {loading && <Loader/>}
+      {todos.length ? (
+      <TodoList todos={todos} onToggle={toggleTodo}/>
+      ) : loading ? null : ( 
+     <p>No todos!</p>
+      )}
     </div>
     </Context.Provider>
   )
--- src/Loader.js
+++ src/Loader.js
@@ -1,5 +1,5 @@
 import React from 'react'
 
-export default () => <div>
-  <div class="lds-grid"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+export default () => <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', marginTop: '100px'}}>
+  <div className="lds-grid"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
 </div>
